b885dac5fd012a14e3badde2ca1bf67a6e366b9d
GitSquared <GitSquared@users.noreply.github.com>
2018-08-08 23:30:30 +0200

    :white_check_mark: Basic tests

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use std::str::FromStr;
 use std::net::IpAddr;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+// I ain't proud of it, but those weird warnings from deep inside Rouille were pissing me off
+#[allow(unreachable_code)]
 fn main() {
     println!("Loading GeoIP database");
     let reader = maxminddb::Reader::open("./GeoLite2-City/GeoLite2-City.mmdb").unwrap();
@@ -61,5 +63,50 @@ fn ip_request_handler(ip: IpAddr, reader: &maxminddb::Reader) -> Response {
             "time": timestamp.as_secs(),
             "api_version": "2.0.0"
         }));
+    }
+}
+
+#[cfg(test)]
+mod database {
+    use super::*;
+
+    #[test]
+    fn can_load() {
+        maxminddb::Reader::open("./GeoLite2-City/GeoLite2-City.mmdb").unwrap();
+    }
+
+    #[test]
+    fn can_lookup() {
+        let reader = maxminddb::Reader::open("./GeoLite2-City/GeoLite2-City.mmdb").unwrap();
+        let _city: geoip2::City = reader.lookup(IpAddr::from_str("8.8.8.8").unwrap()).unwrap();
+    }
+
+    #[test]
+    fn geoip_data_integrity() {
+        let reader = maxminddb::Reader::open("./GeoLite2-City/GeoLite2-City.mmdb").unwrap();
+
+        let country_test: geoip2::City = reader.lookup(IpAddr::from_str("8.8.8.8").unwrap()).unwrap();
+        assert!(country_test.country.unwrap().iso_code.unwrap() == "US");
+
+        let country_test2: geoip2::City = reader.lookup(IpAddr::from_str("193.252.122.73").unwrap()).unwrap();
+        assert!(country_test2.country.unwrap().iso_code.unwrap() == "FR");
+
+        let city_test: geoip2::City = reader.lookup(IpAddr::from_str("172.64.197.5").unwrap()).unwrap();
+        assert!(city_test.city.unwrap().names.unwrap()["en"] == "San Francisco");
+
+        let city_test2: geoip2::City = reader.lookup(IpAddr::from_str("86.119.40.127").unwrap()).unwrap();
+        assert!(city_test2.city.unwrap().names.unwrap()["en"] == "Zurich");
+    }
+}
+
+#[cfg(test)]
+mod request_handler {
+    use super::*;
+
+    #[test]
+    fn returns_400_on_localhost() {
+        let reader = maxminddb::Reader::open("./GeoLite2-City/GeoLite2-City.mmdb").unwrap();
+        let response = ip_request_handler(IpAddr::from_str("127.0.0.1").unwrap(), &reader);
+        assert!(response.status_code == 400);
     }
 }
