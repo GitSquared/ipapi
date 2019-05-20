2f95745c00717593754f41e79472106ac9884026
Gabriel SAILLARD (GitSquared) <gabriel@saillard.dev>
2019-05-20 18:24:25 +0200

    Revert "Limit to 1 db connection by lambda"
    This reverts commit e2580374e139f5cf35eed64d362894058086f105.

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const MongoClient = require('mongodb').MongoClient;
 const dbUrl = process.env.DATABASE_URL;
 const dbName = 'GeoIP';
 const collName = 'City-Blocks-IPv4';
-global.client = null;
-global.data = null;
+const client = new MongoClient(dbUrl);
 
 module.exports = async (req, res) => {
-	if (global.client === null && !global.connecting) {
-		global.connecting = true;
-		global.client = await new MongoClient(dbUrl).connect().catch(err => { throw err });
-		global.data = client.db(dbName).collection(collName);
-		global.connecting = false;
-	}
+	process.once('uncaughtException', err => {
+		res.writeHead(500);
+		res.end(err.message || 'Internal error');
+		process.exit(1);
+	});
+
+
 
 	let ip;
 	if (req.url !== '/') {
@@ -22,7 +22,7 @@ module.exports = async (req, res) => {
 	} else {
 		req.headers['x-forwarded-for'] ? ip = req.headers['x-forwarded-for'] : ip = req.connection.remoteAddress;
 	}
-
+	
 	let v6 = new IPv6(ip);
 	let v4;
 	if (v6.isValid()) {
@@ -30,32 +30,36 @@ module.exports = async (req, res) => {
 	} else {
 		v4 = new IPv4(ip);
 	}
-
+	
 	if (!v4.isValid()) {
 		res.writeHead(400);
 		res.end('Bad IP');
 		return;
 	}
-
+	
 	function error(e) {
 		res.writeHead(500);
 		res.end(e || 'Internal error');
 		return true;
 	}
-
+	
 	let bits = v4.toArray();
 	let rangeString = `${bits[0]}.${bits[1]}.0.0/24`;
-
-	let failed = false;
-	let doc = await global.data.findOne({'network': rangeString}).catch(err => { return error(err.message); });
-	if (failed) return;
-
+	
+	await client.connect().catch(err => { return error(err.message); });
+	
+	let db = client.db(dbName);
+	let data = db.collection(collName);
+	
+	let doc = await data.findOne({'network': rangeString}).catch(err => { return error(err.message); });
+	client.close();
+	
 	if (doc === null) {
 		res.writeHead(404);
 		res.end('IP range not found');
 		return true;
 	}
-
+	
 	res.setHeader('Content-Type', 'application/json;charset=utf-8');
 	res.setHeader('Cache-Control', 's-maxage=172800, maxage=0');
 	res.writeHead(200);
